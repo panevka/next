9629dc0e53627e28492038c09ac8f20c203f1b39
panevka <97028404+panevka@users.noreply.github.com>
2025-12-07 17:44:05 +0100

    feat: update the article collection fields

--- collections/Articles.ts
+++ collections/Articles.ts
@@ -2,11 +2,31 @@ import type { CollectionConfig } from "payload";
 
 export const Articles: CollectionConfig = {
   slug: "articles",
-  auth: true,
   fields: [
     {
       name: "title",
       type: "text",
+    },
+    {
+      name: "description",
+      type: "text",
+    },
+    {
+      name: "content",
+      type: "richText",
+    },
+    {
+      name: "publishedAt",
+      type: "date",
+    },
+    {
+      name: "updatedAt",
+      type: "date",
+    },
+    {
+      name: "cover",
+      type: "upload",
+      relationTo: "media",
     },
   ],
 };
