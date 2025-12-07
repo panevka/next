e36b030fe4c1bf36ea52f374fb9c1c2044b2d862
panevka <97028404+panevka@users.noreply.github.com>
2025-12-07 17:43:41 +0100

    feat: migrate to postgresql due to sqlite issues

--- payload.config.ts
+++ payload.config.ts
@@ -1,4 +1,5 @@
 import { sqliteAdapter } from "@payloadcms/db-sqlite";
+import { postgresAdapter } from "@payloadcms/db-postgres";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
@@ -25,9 +26,9 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, "payload-types.ts"),
   },
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URI || "",
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI,
     },
   }),
   sharp,
